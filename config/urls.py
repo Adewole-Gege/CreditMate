d34fbfd7f31544475daf35b574b8fc2e039e0dba
Adewole-Gege <adewole.gege@ubagroup.com>
2025-06-30 14:12:31 +0100

    initial commit

---
+++ config/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('business.urls')),
+    path('api/', include('ingestion.urls')),
+    path('api/', include('scoring.urls')),
+    path('api/', include('audit.urls')),
+]
