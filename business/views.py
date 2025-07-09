a095633b0dd9748df4a310bd33f17f5a332ec3c2
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2025-07-09 11:26:26 +0000

    set business owner to current user when creating a new business

--- business/views.py
+++ business/views.py
@@ -1,17 +1,7 @@
-from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Business
 from .serializers import BusinessSerializer
-
-class BusinessListCreateView(generics.ListCreateAPIView):
-    queryset = Business.objects.all()
-    serializer_class = BusinessSerializer
-
-class BusinessRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Business.objects.all()
-    serializer_class = BusinessSerializer
-    lookup_field = 'id'
 
 class BusinessViewSet(viewsets.ModelViewSet):
     serializer_class = BusinessSerializer
